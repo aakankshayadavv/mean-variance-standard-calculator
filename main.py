2d4423573ad8c4ef431421d28fed42e977e855fb
aakankshayadavv <akshuyadav66@gmail.com>
2026-06-27 00:11:09 +0530

    feat: complete calculator project

---
+++ main.py
@@ -0,0 +1,8 @@
+import mean_var_std
+from unittest import main
+
+# Test your function manually
+print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+
+# Run automatic unit tests
+main(module='test_module', exit=False)
---
+++ mean_var_std.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+def calculate(list_input):
+    # 1. Validate the input size
+    if len(list_input) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    
+    matrix = np.array(list_input).reshape(3, 3)
+    
+    
+    calculations = {
+        'mean': [
+            matrix.mean(axis=0).tolist(),  # Axis 1 (Columns)
+            matrix.mean(axis=1).tolist(),  # Axis 2 (Rows)
+            matrix.mean().tolist()         # Flattened
+        ],
+        'variance': [
+            matrix.var(axis=0).tolist(),
+            matrix.var(axis=1).tolist(),
+            matrix.var().tolist()
+        ],
+        'standard deviation': [
+            matrix.std(axis=0).tolist(),
+            matrix.std(axis=1).tolist(),
+            matrix.std().tolist()
+        ],
+        'max': [
+            matrix.max(axis=0).tolist(),
+            matrix.max(axis=1).tolist(),
+            matrix.max().tolist()
+        ],
+        'min': [
+            matrix.min(axis=0).tolist(),
+            matrix.min(axis=1).tolist(),
+            matrix.min().tolist()
+        ],
+        'sum': [
+            matrix.sum(axis=0).tolist(),
+            matrix.sum(axis=1).tolist(),
+            matrix.sum().tolist()
+        ]
+    }
+
+    return calculations
